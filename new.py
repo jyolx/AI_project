f496e3c99130de85145370e047f6774fd2f09ca8
jyolx <isac.jyolsnaj@gmail.com>
2024-04-26 15:37:50 +0530

    feat : hvbh algorithm

--- new.py
+++ new.py
@@ -1,47 +1,66 @@
 import random
 import copy
+import math
 
 class beach :
+    
     def __init__(self):
         self.size=10
         self.robot_count=5
         self.collection_point=(0,0)
-        self.debris=[]
+        self.debri_count=5
+        self.debris=random.sample([[(x,y) for y in range(10)] for x in range(10)],self.debri_count)
         self.robot_positions=random.sample([[(x,y) for y in range(10)] for x in range(10)],self.robot_count)
+    
     def show(self):
         pass
-    def mla_star(self,pair):
+    
+    def mla_star(self,robot_pos,debri_pos,t_max):
         pass
+    
     def create_pairs(self,task_list,agent_list):
         pairs=[]
         for t in task_list:
-            for a in agent_list.index():
-                heuristic=abs(t[0]-agent_list[a][0])+abs(t[1]-agent_list[a][1])#add distance to collection
-                pair={"task":t,"agent":a,"h":heuristic}
+            for a in agent_list:
+                heuristic=abs(t["pos"][0]-a["pos"][0])+abs(t["pos"][1]-agent_list["pos"][1])#add distance to collection
+                pair={"debri":t,"agent":a,"h":heuristic}
                 pairs.append(pair)
         pairs.sort(key=lambda x:x["h"])
         return pairs
+    
+    def move_idle_agent(agent_list,a):
+        pass
 
     def task_allocation(self,task_list,agent_list):
-        ongoing_tasks=[]
         while len(task_list)!=0:
+            for i in self.ongoing_tasks:
+                position=i["path"].pop(0)
+                if(len(i["path"])==0):
+                    print(f"Debri {i["debri"]["index"]} cleaned by agent {i["agent"]["index"]}")
+                    self.ongoing_task.remove(i)
+                    task_list.append(i["agent"])
+                if(self.debris[i["debri"]["index"]]==self.robot_positions[i["agent"]["index"]]):
+                    self.debris[i["debri"]["index"]]=position
+                self.robot_positions[i["agent"]["index"]]=position  
+            self.show()
             pairs=self.create_pairs(task_list,agent_list)
             for pair in pairs:
-                if(pair["agent"] in agent_list.index() and pair["task"] in task_list):
-                    path_time=self.mla_star(pair)
-                    ongoing_tasks.append(path_time)
+                if(pair["agent"] in agent_list and pair["task"] in task_list):
+                    path_time=self.mla_star(pair["agent"]["pos"],pair["task"]["pos"],math.inf)
+                    task={"agent":pair["agent"],"debri":pair["debri"],"path":path_time}
+                    self.ongoing_tasks.append(task)
                     agent_list.pop(pair["agent"])
                     task_list.remove(pair["task"])
             for a in agent_list.index:
-                pass
-
-            
-        
+                if agent_list[a] in task_list :
+                    self.move_idle_agent(agent_list,a)
+            self.time_step=self.time_step+1
+             
     def simulate(self):
         self.time_step=0
-        task_list=copy.deepcopy(self.debris)
-        agent_list={f"agent{i}":x for i,x in enumerate(self.robot_positions)}
-
+        task_list=[{"index":i,"pos":x} for i,x in enumerate(self.debris)]
+        agent_list=[{"index":i,"pos":x} for i,x in enumerate(self.robot_positions)]
+        self.ongoing_task=[]
         self.task_allocation()
 
 
